cd012c4749a6a52ac9043840e2d26d66cef8e3a2
ckorikov <constantine.korikov@gmail.com>
2018-06-04 22:13:52 +0300

    made some reformat

--- cpp_bridge/test.cpp
+++ cpp_bridge/test.cpp
@@ -14,35 +14,38 @@ extern "C"
 #endif
 
 
-DLLEXPORT mint WolframLibrary_getVersion () {
+DLLEXPORT mint WolframLibrary_getVersion ()
+{
     return WolframLibraryVersion;
-  }
+}
 
-DLLEXPORT int WolframLibrary_initialize ( WolframLibraryData libData) {
-  	return 0;
-  }
+DLLEXPORT int WolframLibrary_initialize ( WolframLibraryData libData)
+{
+    return 0;
+}
 
-DLLEXPORT void WolframLibrary_uninitialize (
-  WolframLibraryData libData) {
-  	return;
-  }
+DLLEXPORT void WolframLibrary_uninitialize (WolframLibraryData libData)
+{
+    return;
+}
 
-DLLEXPORT int constantzero (WolframLibraryData libData, mint Argc,
-  MArgument *Args, MArgument Res) {
-     MArgument_setInteger (Res, 0);
-     return LIBRARY_NO_ERROR;
-  }
+DLLEXPORT int constantzero (WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res)
+{
+    MArgument_setInteger (Res, 0);
+    return LIBRARY_NO_ERROR;
+}
 
-DLLEXPORT int hello (WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res) {
-  	mint I0;
-  	mint I1;
+DLLEXPORT int hello (WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res)
+{
+    mint I0;
+    mint I1;
     mint I2;
-  	I0 = MArgument_getInteger (Args[0]);
+    I0 = MArgument_getInteger (Args[0]);
     I1 = MArgument_getInteger (Args[1]);
-  	I2 = MathLibrary::add(I0, I1);
-  	MArgument_setInteger (Res, I2);
-  	return LIBRARY_NO_ERROR;
-  }
+    I2 = MathLibrary::add(I0, I1);
+    MArgument_setInteger (Res, I2);
+    return LIBRARY_NO_ERROR;
+}
 
 #ifdef __cplusplus
 }
